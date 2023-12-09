67f5a5d2bc17db7dbc890fba31bbb37517f14a78
John Arnold <photowalkthrough@gmail.com>
2023-12-09 13:49:20 +0000

    day 9 - decided to go as succinct as possible

---
+++ day 9/d9p1.py
@@ -0,0 +1,9 @@
+# open and read the input file input a list of lists of integers
+sequences = [[int(n) for n in line.split()] for line in open("input.txt", "r").read().split("\n")]
+
+# Return the next value in the sequence. Uses recursion - see d9p2.py for an explanation of this
+def find_next_value(sequence):
+    return sequence[-1] + (0 if all(v == 0 for v in sequence) else find_next_value([sequence[i] - sequence[i-1] for i in range(1, len(sequence))]))
+
+# Iterate the list of sequences using find_next_value() to find the next value. Add those values together and print it
+print(f"Total: {sum([find_next_value(sequence) for sequence in sequences])}")
---
+++ day 9/d9p2.py
@@ -0,0 +1,29 @@
+# open and read the input file input a list of lists of integers
+sequences = [[int(n) for n in line.split()] for line in open("input.txt", "r").read().split("\n")]
+
+# Return the prev value in the sequence. Uses recursion
+def find_prev_value(sequence):
+    return sequence[0] - (0 if all(v == 0 for v in sequence) else find_prev_value([sequence[i] - sequence[i-1] for i in range(1, len(sequence))]))
+
+# Iterate the list of sequences using find_prev_value() to find the next value. Add those values together and print it
+print(f"Total: {sum([find_prev_value(sequence) for sequence in sequences])}")
+
+
+
+
+
+
+
+
+# Just to explain the code above: find_prev_values is basically this:
+#
+# def find_prev_value(sequence):
+#     if(all(v == 0 for v in sequence)):
+#         # If all values in sequence are 0 then the previous value is 0
+#         return 0
+#     else:       
+#         # Return our first sequence number - the previous value in the list
+#         new_sequence = [sequence[i] - sequence[i-1] for i in range(1, len(sequence))]
+#         return sequence[0] - find_prev_value(new_sequence)
+#
+# For find_next_value we just add to the last value in the sequence instead of subtracting from the first
