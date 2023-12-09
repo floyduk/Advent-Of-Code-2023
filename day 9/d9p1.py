f9915b718189b6ef17d421df9e5ba2bd8e951c51
John Arnold <photowalkthrough@gmail.com>
2023-12-09 14:06:04 +0000

    shortened a line and added more explanation to p2

--- day 9/d9p1.py
+++ day 9/d9p1.py
@@ -2,8 +2,8 @@
 sequences = [[int(n) for n in line.split()] for line in open("input.txt", "r").read().split("\n")]
 
 # Return the next value in the sequence. Uses recursion - see d9p2.py for an explanation of this
-def find_next_value(sequence):
-    return sequence[-1] + (0 if all(v == 0 for v in sequence) else find_next_value([sequence[i] - sequence[i-1] for i in range(1, len(sequence))]))
+def find_next_value(s):
+    return s[-1] + (0 if all(v == 0 for v in s) else find_next_value([s[i] - s[i-1] for i in range(1, len(s))]))
 
 # Iterate the list of sequences using find_next_value() to find the next value. Add those values together and print it
 print(f"Total: {sum([find_next_value(sequence) for sequence in sequences])}")
--- day 9/d9p2.py
+++ day 9/d9p2.py
@@ -2,8 +2,8 @@
 sequences = [[int(n) for n in line.split()] for line in open("input.txt", "r").read().split("\n")]
 
 # Return the prev value in the sequence. Uses recursion
-def find_prev_value(sequence):
-    return sequence[0] - (0 if all(v == 0 for v in sequence) else find_prev_value([sequence[i] - sequence[i-1] for i in range(1, len(sequence))]))
+def find_prev_value(s):
+    return s[0] - (0 if all(v == 0 for v in s) else find_prev_value([s[i] - s[i-1] for i in range(1, len(s))]))
 
 # Iterate the list of sequences using find_prev_value() to find the next value. Add those values together and print it
 print(f"Total: {sum([find_prev_value(sequence) for sequence in sequences])}")
@@ -27,3 +27,10 @@ print(f"Total: {sum([find_prev_value(sequence) for sequence in sequences])}")
 #         return sequence[0] - find_prev_value(new_sequence)
 #
 # For find_next_value we just add to the last value in the sequence instead of subtracting from the first
+# 
+#       [s[i] - s[i-1] for i in range(1, len(s))]
+# Takes a sequence "s" and uses a list comprehension to return the differences between the values
+# 
+#       all(v == 0 for v in s)
+# Returns true if all values v in sequence s are equal to 0
+
